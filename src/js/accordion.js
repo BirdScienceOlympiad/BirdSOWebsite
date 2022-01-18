2b84bc341c1f5dd6f7c3661d75376c829a376443
georgesun <georgemarksun@gmail.com>
2022-01-18 01:02:10 -0800

    Add keyboard control for accordion and footer

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,13 +1,22 @@
 $(function() {
 	var questions = document.querySelectorAll('.accordion > h3');
+	function accordion_toggle(e) {
+		e.classList.toggle('active');
+		var panel = e.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + 'px';
+		}
+	}
 	for (var i = 0; i < questions.length; i++) {
+		questions[i].setAttribute('tabindex', '0');
 		questions[i].addEventListener('click', function() {
-			this.classList.toggle('active');
-			var panel = this.nextElementSibling;
-			if (panel.style.maxHeight) {
-				panel.style.maxHeight = null;
-			} else {
-				panel.style.maxHeight = panel.scrollHeight + 'px';
+			accordion_toggle(this);
+		});
+		questions[i].addEventListener('keypress', function(event) {
+			if (event.keyCode == 13) {
+				accordion_toggle(this);
 			}
 		});
 	}
